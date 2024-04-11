48fcb9c9832e0a2547b22243a7d89e1deccb0078
akki <takiyamaakihito@gmail.com>
2024-04-12 00:37:19 +0900

    四季報オンラインにログインしたい

--- scraping.py
+++ scraping.py
@@ -1,19 +1,38 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 # chromedriverのパスを指定
 driver = webdriver.Chrome()
 
-# Qiitaトップページにアクセス
-url = "https://qiita.com/"
+# 四季報オンライン
+url = "https://shikiho.toyokeizai.net/"
 driver.get(url)
 
-xpath = r'/html/body/div[1]/div[1]/header/div/div[2]/a[1]'
-element = driver.find_element(By.XPATH, xpath)
+login = r'/html/body/div[2]/div/div/header/div/div[3]/div[3]/button'
+element1 = driver.find_element(By.XPATH, login)
+element1.click()
+time.sleep(5)
 
-# element = WebDriverWait(driver, 10).until(
-#     EC.presence_of_element_located((By.XPATH, xpath))
-# )
-element.click()
+login2 = r'/html/body/div[2]/div/div/header/div/div[3]/div[3]/div/button'
+element2 = driver.find_element(By.XPATH, login2)
+element2.click()
+time.sleep(5)
+
+mail = r'/html/body/app-main/app-widget/screen-layout/main/current-screen/div/screen-login/p[4]/input'
+gmail = "hoge@mail.com"
+mail = driver.find_element(By.NAME, "email")
+mail.send_keys(gmail)
+time.sleep(5)
+
+password = r'/html/body/app-main/app-widget/screen-layout/main/current-screen/div/screen-login/p[5]/input'
+my_password = "MyPassword1"
+element4 = driver.find_element(By.XPATH, password)
+element4.send_keys(my_password)
+time.sleep(5)
+
+login3 = r'/html/body/app-main/app-widget/screen-layout/main/current-screen/div/screen-login/p[7]/button'
+element5 = driver.find_element(By.XPATH, login3)
+element5.click()
+time.sleep(5)
